1fd35dce2b6aac6322eae8f0a2fc61110420e4a2
Astrid Aguilar <mixaguilar@yahoo.com>
2021-05-06 03:27:28 +0000

    stdin setup for user input

--- play.js
+++ play.js
@@ -2,6 +2,25 @@
 
 const connect = require("./client");
 
+// -------------- USER INTERFACE SET UP -------------- //
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+
+  
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
 // ---------------------------------- //
 
 console.log("Connecting ...");
